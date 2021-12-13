9920bf90028c04693d26edb9f28bf97022a2caa5
Lynncept77 <95460649+Lynncept77@users.noreply.github.com>
2021-12-14 00:37:08 +0530

    Update alive.py

--- lunaBot/modules/alive.py
+++ lunaBot/modules/alive.py
@@ -17,16 +17,16 @@ async def awake(event):
 
  Miss_Akshi = event.sender.first_name
 
- Miss_Akshi = "♡ I,m Miss_Akshi💕 \n\n"
+ Miss_Akshi = "♡ I'm Tanjirou Kamado \n\n"
 
  Miss_Akshi += "♡ I'm Working with awesome speed**\n\n"
 
- Miss_Akshi += "**♡Miss_Akshi : 1.0 LATEST**\n\n"
+ Miss_Akshi += "**♡Tanjirou_Kamado : 1.0 LATEST**\n\n"
 
- Miss_Akshi += "**♡ 𝘔𝘺 𝘰𝘸𝘯𝘦𝘳 : [𝘈𝘴𝘩𝘶👑](t.me/Professer_Ashu)\n\n"
+ Miss_Akshi += "**♡ 𝘔𝘺 𝘰𝘸𝘯𝘦𝘳 : [Tanjirou👑](t.me/Lynncept_Content)\n\n"
 
  Miss_Akshi += "**♡ Telethon Version : 1.23.0**\n\n"
 
- BUTTON = [[Button.url("𝘚𝘶𝘱𝘱𝘰𝘳𝘵🔥", "https://t.me/Miss_AkshiV1_Support"), Button.url("𝘜𝘱𝘥𝘢𝘵𝘦𝘴", "https://t.me/Miss_AkshiV1_Updates")]]
+ BUTTON = [[Button.url("𝘚𝘶𝘱𝘱𝘰𝘳𝘵🔥", "https://t.me/Tanji_Kamado_Support"), Button.url("𝘜𝘱𝘥𝘢𝘵𝘦𝘴", "https://t.me/Tanji_updates")]]
 
  await tbot.send_file(event.chat_id, PHOTO, caption=Miss_Akshi,  buttons=BUTTON)
