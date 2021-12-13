96cacd01975ad1a5863ffb8d0d757b392bd0ff64
Lynncept77 <95460649+Lynncept77@users.noreply.github.com>
2021-12-13 23:47:37 +0530

    Update alive.py

--- lunaBot/modules/alive.py
+++ lunaBot/modules/alive.py
@@ -6,11 +6,11 @@ from lunaBot import telethn as tgbot
 PHOTO = "https://telegra.ph/file/2cc96af4c1a40f44260cd.mp4"
 @register(pattern=("/alive"))
 async def awake(event):
- Miss_Akshi = event.sender.first_name
- Miss_Akshi = "🌞 I,m Tanjirou Kamado \n\n"
- Miss_Akshi += "🌞 I'm Working with awesome speed**\n\n"
- Miss_Akshi += "🌞 Miss_Akshi : 1.0 LATEST**\n\n"
- Miss_Akshi += "🌞  𝘔𝘺 𝘰𝘸𝘯𝘦𝘳 : [Tanjirou](t.me/Lynncept77)\n\n"
- Miss_Akshi += "🌞 Telethon Version : 1.23.0**\n\n"
+ Tanjirou_Kamado = event.sender.first_name
+ Tanjirou_Kamado = "🌞 I,m Tanjirou Kamado \n\n"
+ Tanjirou_Kamado += "🌞 I'm Working with awesome speed**\n\n"
+ Tanjirou_Kamado += "🌞 Miss_Akshi : 1.0 LATEST**\n\n"
+ Tanjirou_Kamado += "🌞  𝘔𝘺 𝘰𝘸𝘯𝘦𝘳 : [Tanjirou](t.me/Lynncept77)\n\n"
+ Tanjirou_Kamado += "🌞 Telethon Version : 1.23.0**\n\n"
  BUTTON = [[Button.url("𝘚𝘶𝘱𝘱𝘰𝘳𝘵🔥", "https://t.me/Tanji_kamado_support"), Button.url("𝘜𝘱𝘥𝘢𝘵𝘦𝘴", "https://t.me/Tanji_Updates)]]
  await tbot.send_file(event.chat_id, PHOTO, caption=Miss_Akshi,  buttons=BUTTON)
