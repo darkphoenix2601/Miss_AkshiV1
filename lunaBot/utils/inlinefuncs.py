c147cb2c045adb42e7eb62d0c7d79d21c5c3450d
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-18 20:04:35 +0530

    Update inlinefuncs.py

--- lunaBot/utils/inlinefuncs.py
+++ lunaBot/utils/inlinefuncs.py
@@ -433,8 +433,8 @@ async def tg_search_func(answers, text, user_id):
 
 
 async def music_inline_func(answers, query):
-    chat_id = -1001445180719
-    group_invite = "https://t.me/joinchat/vSDE2DuGK4Y4Nzll"
+    chat_id =  -1001678954822
+    group_invite = "https://t.me/+9sNFQWMmlUA4ZTNh"
     try:
         messages = [
             m
